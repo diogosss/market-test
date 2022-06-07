de84095e61ae47b94e4007a439e232de3b076ad6
Diego Salazar <diogol11ldu20@hotmail.com>
2022-06-07 18:55:55 -0500

    [Lesson 21] Qué es el patrón Data Mapper y qué resuelve

---
+++ src/main/java/com/diogoslz/marketplace/domain/Product.java
@@ -0,0 +1,68 @@
+package com.diogoslz.marketplace.domain;
+
+public class Product {
+    private int productId;
+    private String nombre;
+    private int categoryId;
+    private double price;
+    private int stock;
+    private boolean state;
+    //no incluye codigo de barras por diseño
+    private Category category;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public boolean isState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
