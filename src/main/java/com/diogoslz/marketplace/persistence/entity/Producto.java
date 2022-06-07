0290470f91996fd919f21c81de00b0bd783c7c6c
Diego Salazar <diogol11ldu20@hotmail.com>
2022-06-07 14:10:25 -0500

    [Lesson15] Mapear Tablas como clases

---
+++ src/main/java/com/diogoslz/marketplace/persistence/entity/Producto.java
@@ -0,0 +1,85 @@
+package com.diogoslz.marketplace.persistence.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="productos")
+public class Producto {
+
+    @Id  //clave primaria
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto") //nombre ral en la tabla de datos abajo notacion camelcase
+    private Integer idProducto;
+
+    private String nombre; //es el mismo nombre valor de la tabla no se pone @Column
+
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+
+    @Column(name = "codigo_barras")
+    private String codigoBarras;
+
+    @Column(name = "precio_venta")
+    private Double precioVenta;
+
+    @Column(name = "cantidad_stock")
+    private Integer cantidadStock;
+
+    private boolean estado; //es el mismo nombre valor de la tabla no se pone @Column
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getCodigoBarras() {
+        return codigoBarras;
+    }
+
+    public void setCodigoBarras(String codigoBarras) {
+        this.codigoBarras = codigoBarras;
+    }
+
+    public Double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(Double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public Integer getCantidadStock() {
+        return cantidadStock;
+    }
+
+    public void setCantidadStock(Integer cantidadStock) {
+        this.cantidadStock = cantidadStock;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+}
