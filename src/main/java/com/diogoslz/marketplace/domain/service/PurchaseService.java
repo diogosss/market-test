c584a63bdd2025cd29cc7faf19ce67af1a27b0fe
Diego Salazar <diogol11ldu20@hotmail.com>
2022-06-13 10:38:31 -0500

    [Reto] Service y WebController Purchase

---
+++ src/main/java/com/diogoslz/marketplace/domain/service/PurchaseService.java
@@ -0,0 +1,30 @@
+package com.diogoslz.marketplace.domain.service;
+
+import com.diogoslz.marketplace.domain.Purchase;
+import com.diogoslz.marketplace.domain.repository.PurchaseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PurchaseService {
+
+    @Autowired
+    private PurchaseRepository purchaseRepository;
+
+    public List<Purchase> getAll(){
+        return purchaseRepository.getAll();
+    }
+
+    public Optional<List<Purchase>> getByClient(String clienteId){
+        return purchaseRepository.getByClient(clienteId);
+    }
+
+    public Purchase save(Purchase purchase){
+        return purchaseRepository.save(purchase);
+    }
+
+
+}
