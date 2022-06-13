e3dda47f85b7d029f428cc2d4e9d6e037f84ef73
Diego Salazar <diogol11ldu20@hotmail.com>
2022-06-13 10:50:55 -0500

    [Fixed] Correo Electronico en Cliente.java

---
+++ src/main/generated/com/diogoslz/marketplace/persistence/mapper/PurchaseItemMapperImpl.java
@@ -0,0 +1,84 @@
+package com.diogoslz.marketplace.persistence.mapper;
+
+import com.diogoslz.marketplace.domain.PurchaseItem;
+import com.diogoslz.marketplace.persistence.entity.ComprasProducto;
+import com.diogoslz.marketplace.persistence.entity.ComprasProductoPK;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-06-13T10:41:51-0500",
+    comments = "version: 1.5.1.Final, compiler: javac, environment: Java 11.0.15 (Eclipse Adoptium)"
+)
+@Component
+public class PurchaseItemMapperImpl implements PurchaseItemMapper {
+
+    @Override
+    public PurchaseItem toPurchaseItem(ComprasProducto producto) {
+        if ( producto == null ) {
+            return null;
+        }
+
+        PurchaseItem purchaseItem = new PurchaseItem();
+
+        Integer idProducto = productoIdIdProducto( producto );
+        if ( idProducto != null ) {
+            purchaseItem.setProductId( idProducto );
+        }
+        if ( producto.getCantidad() != null ) {
+            purchaseItem.setQuantity( producto.getCantidad() );
+        }
+        if ( producto.getEstado() != null ) {
+            purchaseItem.setActive( producto.getEstado() );
+        }
+        if ( producto.getTotal() != null ) {
+            purchaseItem.setTotal( producto.getTotal() );
+        }
+
+        return purchaseItem;
+    }
+
+    @Override
+    public ComprasProducto toComprasProducto(PurchaseItem item) {
+        if ( item == null ) {
+            return null;
+        }
+
+        ComprasProducto comprasProducto = new ComprasProducto();
+
+        comprasProducto.setId( purchaseItemToComprasProductoPK( item ) );
+        comprasProducto.setCantidad( item.getQuantity() );
+        comprasProducto.setEstado( item.isActive() );
+        comprasProducto.setTotal( item.getTotal() );
+
+        return comprasProducto;
+    }
+
+    private Integer productoIdIdProducto(ComprasProducto comprasProducto) {
+        if ( comprasProducto == null ) {
+            return null;
+        }
+        ComprasProductoPK id = comprasProducto.getId();
+        if ( id == null ) {
+            return null;
+        }
+        Integer idProducto = id.getIdProducto();
+        if ( idProducto == null ) {
+            return null;
+        }
+        return idProducto;
+    }
+
+    protected ComprasProductoPK purchaseItemToComprasProductoPK(PurchaseItem purchaseItem) {
+        if ( purchaseItem == null ) {
+            return null;
+        }
+
+        ComprasProductoPK comprasProductoPK = new ComprasProductoPK();
+
+        comprasProductoPK.setIdProducto( purchaseItem.getProductId() );
+
+        return comprasProductoPK;
+    }
+}
