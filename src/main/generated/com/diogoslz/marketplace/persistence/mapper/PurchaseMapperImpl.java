7b13e4cc0d3dc12da254a2a9ecfa285f51c0b204
Diego Salazar <diogol11ldu20@hotmail.com>
2022-06-13 15:34:00 -0500

    [Lesson36] Autenticacion con JWT

--- src/main/generated/com/diogoslz/marketplace/persistence/mapper/PurchaseMapperImpl.java
+++ src/main/generated/com/diogoslz/marketplace/persistence/mapper/PurchaseMapperImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-06-13T14:53:45-0500",
+    date = "2022-06-13T15:29:16-0500",
     comments = "version: 1.5.1.Final, compiler: javac, environment: Java 11.0.15 (Eclipse Adoptium)"
 )
 @Component
--- src/main/java/com/diogoslz/marketplace/web/security/SecurityConfig.java
+++ src/main/java/com/diogoslz/marketplace/web/security/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.diogoslz.marketplace.web.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,5 +19,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         //super.configure(auth);
         auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //super.configure(http);
+        http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()
+                .anyRequest().authenticated();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
